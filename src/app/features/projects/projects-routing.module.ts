8101542020c0e2278919641901e9302205656597
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2021-11-11 20:34:52 +0400

    update UI,

--- src/app/features/projects/projects-routing.module.ts
+++ src/app/features/projects/projects-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes =[
+const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ProjectsComponent } from './projects/projects.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ProjectsRoutingModule } from './projects-routing.module';
 
 @NgModule({
   declarations: [
     ProjectsComponent
   ],
   imports: [
-    SharedModule
+    SharedModule,
+    ProjectsRoutingModule
   ]
 })
 export class ProjectsModule { }
